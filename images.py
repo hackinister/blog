1931ca3b01ddb0d150fdefca927328b8e946a977
Christoph Timm <ctimm@datenschutz-nord.de>
2026-02-17 22:14:23 +0100

    New Blog Post on 2026-02-17 22:14:23

--- images.py
+++ images.py
@@ -1,6 +1,7 @@
 import os
 import re
 import shutil
+import subprocess
 
 # Paths
 posts_dir = "/home/ctimm/Development/Blog/content/posts/"
@@ -18,7 +19,7 @@ for filename in os.listdir(posts_dir):
 
         for image in images:
             new_filename = image.replace(" ", "-")
-            markdown_image = f"[Image Description](/img/{new_filename})"
+            markdown_image = f"![Image Description](/images/{new_filename})"
             content = content.replace(f"[[{image}]]", markdown_image)
 
             image_source = os.path.join(attachment_dir, image)
@@ -29,3 +30,35 @@ for filename in os.listdir(posts_dir):
             file.write(content)
 
 print("Markdown files processed and images copied successfully.")
+
+# Cleanup unused images
+print("Cleaning up unused images...")
+search_dirs = [
+    "/home/ctimm/Development/Blog/content",
+    "/home/ctimm/Development/Blog/layouts",
+    "/home/ctimm/Development/Blog/themes",
+    "/home/ctimm/Development/Blog/config",
+    "/home/ctimm/Development/Blog/archetypes",
+]
+existing_search_dirs = [d for d in search_dirs if os.path.exists(d)]
+
+if os.path.exists(static_images_dir):
+    for filename in os.listdir(static_images_dir):
+        if filename.lower().endswith(
+            (".png", ".webp", ".jpg", ".jpeg", ".gif", ".svg")
+        ):
+            filepath = os.path.join(static_images_dir, filename)
+            result = subprocess.run(
+                ["grep", "-r", "-q", "-F", filename] + existing_search_dirs,
+                stdout=subprocess.DEVNULL,
+                stderr=subprocess.DEVNULL,
+            )
+            if result.returncode == 1:
+                print(f"Deleting unused image: {filename}")
+                os.remove(filepath)
+            elif result.returncode > 1:
+                print(
+                    f"Error checking usage for {filename}. grep returned {result.returncode}"
+                )
+
+print("Unused images cleaned up successfully.")
